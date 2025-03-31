3eee70c68dbf5af52dbf91c0485b363dc988b519
adriankarlen <113891532+adriankarlen@users.noreply.github.com>
2025-03-31 12:55:46 +0200

    style(nvim): formatting

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -1,44 +1,42 @@
 return {
   "stevearc/conform.nvim",
   cond = not vim.g.vscode,
-  
+
   event = { "BufWritePre" },
-  config = function()
-    require("conform").setup {
-      quiet = true,
-      lsp_fallback = true,
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettier", "eslint", stop_after_first = true },
-        typescript = { "prettier", "eslint", stop_after_first = true },
-        javascriptreact = { "prettier", "rustywind" },
-        typescriptreact = { "prettier", "rustywind" },
-        svelte = { "prettier", "rustywind" },
-        html = { "prettier", "rustywind" },
-        css = { "prettier" },
-        scss = { "prettier" },
-        less = { "prettier" },
-        json = { "prettier" },
-        jsonc = { "prettier" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        mdx = { "prettier" },
-        graphql = { "prettier" },
-        go = { "gofmt"},
-        cs = { "csharpier" },
-        xml = { "xmlformat" },
-        svg = { "xmlformat" },
-        rust = { "rustfmt" },
+  opts = {
+    quiet = true,
+    lsp_fallback = true,
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettier", "eslint", stop_after_first = true },
+      typescript = { "prettier", "eslint", stop_after_first = true },
+      javascriptreact = { "prettier", "rustywind" },
+      typescriptreact = { "prettier", "rustywind" },
+      svelte = { "prettier", "rustywind" },
+      html = { "prettier", "rustywind" },
+      css = { "prettier" },
+      scss = { "prettier" },
+      less = { "prettier" },
+      json = { "prettier" },
+      jsonc = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      mdx = { "prettier" },
+      graphql = { "prettier" },
+      go = { "gofmt" },
+      cs = { "csharpier" },
+      xml = { "xmlformat" },
+      svg = { "xmlformat" },
+      rust = { "rustfmt" },
+    },
+    formatters = {
+      xmlformat = {
+        cmd = { "xmlformat" },
+        args = { "--selfclose", "-" },
       },
-      formatters = {
-        xmlformat = {
-          cmd = { "xmlformat" },
-          args = { "--selfclose", "-" },
-        },
-        injected = { options = { ignore_errors = false } },
-      },
-    }
-  end,
+      injected = { options = { ignore_errors = false } },
+    },
+  },
   keys = {
     {
       "<leader>cf",
