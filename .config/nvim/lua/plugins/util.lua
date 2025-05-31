106ee7f78145b7b96a908978accb4b4c4dabf61b
adriankarlen <113891532+adriankarlen@users.noreply.github.com>
2025-05-31 11:34:28 +0200

    feat(nvim): replace hurl with kulala

--- .config/nvim/lua/plugins/util.lua
+++ .config/nvim/lua/plugins/util.lua
@@ -17,47 +17,18 @@ return {
     },
   },
   {
-    "jellydn/hurl.nvim",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
-    },
-    ft = "hurl",
-    opts = {
-      show_notification = true,
-      mode = "popup",
-      env_file = {
-        "vars.env",
-        "env/at.env",
-      },
-    },
+    "mistweaverco/kulala.nvim",
     keys = {
-      -- Run API request
-      { "<leader>hr", "<cmd>HurlRunnerAt<CR>", desc = "run api request", ft = "hurl" },
-      { "<leader>ha", "<cmd>HurlRunner<CR>", desc = "run all requests", ft = "hurl" },
-      { "<leader>hA", "<cmd>HurlVerbose<CR>", desc = "run api in verbose mode", ft = "hurl" },
-      { "<leader>he", "<cmd>HurlRunnerToEntry<CR>", desc = "run api request to entry", ft = "hurl" },
-      { "<leader>ht", "<cmd>HurlToggleMode<CR>", desc = "toggle popup/split result", ft = "hurl" },
-      {
-        "<leader>hv",
-        function()
-          local var_name = vim.fn.input "Enter env variable name: "
-          local var_value = vim.fn.input "Enter env variable value: "
-          if var_name ~= "" and var_value ~= "" then
-            vim.cmd("HurlSetVariable " .. var_name .. " " .. var_value)
-          end
-        end,
-        desc = "add env variable",
-        ft = "hurl",
-      },
-      { "<leader>hm", "<cmd>HurlManageVariable<cr>", desc = "manage variable", ft = "hurl" },
-      -- Run Hurl request in visual mode
-      { "<leader>h", ":HurlRunner<CR>", desc = "Hurl Runner", mode = "v", ft = "hurl" },
+      { "<leader>rs", desc = "send request" },
+      { "<leader>ra", desc = "send all requests" },
+      { "<leader>rb", desc = "open scratchpad" },
     },
+    ft = { "http", "rest" },
+    opts = {},
   },
   {
-    "folke/lazydev.nvim",
+    "Jari27/lazydev.nvim",
+    branch = "deprecate_client_notify",
     ft = "lua",
     cmd = "LazyDev",
     opts = {
