fd9c8fc0c70faaa6f68f97eb3200c720195722f2
adriankarlen <113891532+adriankarlen@users.noreply.github.com>
2025-06-04 10:43:01 +0200

    fix(nvim): remove unused function

--- .config/nvim/lua/utils/fn.lua
+++ .config/nvim/lua/utils/fn.lua
@@ -1,12 +1,7 @@
 local M = {}
-local uv = vim.loop
 
-M.is_win = function()
-  uv.os_uname().version:match "Windows"
-end
-
+local git_path = vim.fn.finddir(".git", ".;")
 M.root = function()
-  local git_path = vim.fn.finddir(".git", ".;")
   return vim.fn.fnamemodify(git_path, ":h")
 end
 
